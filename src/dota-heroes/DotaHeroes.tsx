2d955045dc00166b3ad1233333b4f6d7785c42f2
Yuki XD <pxnditxyr@gmail.com>
2021-10-24 00:29:48 -0400

    Initializing Project Dota Heroes App | React Typescript

---
+++ src/dota-heroes/DotaHeroes.tsx
@@ -0,0 +1,9 @@
+
+export const DotaHeroes : React.FC = () => {
+    return (
+        <>
+            <h1> Dota Heroes </h1>
+            <hr />
+        </>
+    );
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+import { DotaHeroes } from './dota-heroes/DotaHeroes';
+
+import './index.css'
+
+ReactDOM.render(
+    <DotaHeroes />,
+    document.querySelector( "#root" ),
+);
